414efc9654227c66d54d28ae9468144bb3cf8828
Anees K <aneesanz221@gmail.com>
2023-03-10 17:18:45 +0530

    handle cart and checkout for logged in users

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from store.models import Product, Variation
 
 # Create your models here.
+
+class Wishlist(models.Model):
+    wishlist_id = models.CharField(max_length=250, blank=True)
+    date_added = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return str(self.wishlist_id)
+    
+
+class WishlistItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variations = models.ManyToManyField(Variation, blank=True)
+    wishlist = models.ForeignKey(Wishlist, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.product
--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.wishlist,name="wishlist"),
+    path('add_wishlist/<int:product_id>/', views.add_wishlist,name="add_wishlist"),
+    path('remove_wishlist/<int:product_id>/', views.remove_wishlist,name="remove_wishlist"),
 ] 
